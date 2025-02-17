b552cae94da7986e43c320315d05704cf17ad8ac
kyddaniel <mksuenaa@connect.ust.hk>
2025-02-17 21:36:57 +0800

    Added TODOs

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,10 @@ function App() {
 }
 
 export default App
+
+// TODOs
+// Add components to display header, fetch user input & output results  table
+// Fetch and store user input
+// Derive investment results with the utility functions
+// Display results in HTML table (<table> <thead> <tbody> <tr> <th> <td>)
+// Conditionally display info message if invalid duration was entered
